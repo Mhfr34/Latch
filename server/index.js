c359518f85311aebcfcee6247c0d12a576e54afb
Mhamad  Farhat <mfarhat26@gmail.com>
2025-06-22 10:23:42 +0300

    whatsapp integration

--- server/index.js
+++ server/index.js
@@ -115,7 +115,7 @@ const startServerAndWhatsApp = async () => {
                   driver.nextSubscriptionDate
                 );
                 const twoAndHalfHoursFromNow = new Date(
-                  currentTime.getTime() + 2.5 * 60 * 60 * 1000
+                  currentTime.getTime() + 19.5 * 60 * 60 * 1000
                 );
 
                 if (
